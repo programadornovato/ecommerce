46e1c2ea79b6f7c9e18b1b9caa683913b5575e58
Programador Novato <info@programadornovato.com>
2020-01-31 03:21:38 -0600

    Ecommerce con PHP🐘 y MySql🐬 [33.-Imprimir nuestra factura en PDF📜 2da parte]
    Video:
    https://youtu.be/kXiJVKM-wWo

--- imprimirFactura.php
+++ imprimirFactura.php
@@ -3,27 +3,28 @@ session_start();
 include_once "admin/db_ecommerce.php";
 $con = mysqli_connect($host, $user, $pass, $db);
 
-$queryRecibe="SELECT nombre,email,direccion 
+$queryRecibe = "SELECT nombre,email,direccion 
 from recibe 
-where idCli='".$_SESSION['idCliente']."';";
-$resRecibe=mysqli_query($con,$queryRecibe);
-$rowRecibe=mysqli_fetch_assoc($resRecibe);
+where idCli='" . $_SESSION['idCliente'] . "';";
+$resRecibe = mysqli_query($con, $queryRecibe);
+$rowRecibe = mysqli_fetch_assoc($resRecibe);
 
-$queryCli="SELECT nombre,email,direccion 
+$queryCli = "SELECT nombre,email,direccion 
 from clientes 
-where id='".$_SESSION['idCliente']."';";
-$resCli=mysqli_query($con,$queryCli);
-$rowCli=mysqli_fetch_assoc($resCli);
+where id='" . $_SESSION['idCliente'] . "';";
+$resCli = mysqli_query($con, $queryCli);
+$rowCli = mysqli_fetch_assoc($resCli);
 
-$idVenta= mysqli_real_escape_string($con,$_REQUEST['idVenta']??'');
-$queryVenta="SELECT v.id,v.fecha
+$idVenta = mysqli_real_escape_string($con, $_REQUEST['idVenta'] ?? '');
+$queryVenta = "SELECT v.id,v.fecha
 FROM ventas AS v
 WHERE v.id = '$idVenta';";
-$resVenta=mysqli_query($con,$queryVenta);
-$rowVenta=mysqli_fetch_assoc($resVenta);
+$resVenta = mysqli_query($con, $queryVenta);
+$rowVenta = mysqli_fetch_assoc($resVenta);
 ?>
+<?php ob_start(); ?>
 <div>
-    <img src="admin/images/pn.png" style="width: 30px;" >
+    <img src="admin/images/pn.png" style="width: 30px;">
     My eccomerce
 </div>
 
@@ -54,3 +55,56 @@ $rowVenta=mysqli_fetch_assoc($resVenta);
         </tr>
     </tbody>
 </table>
+<table style="width: 750px;margin-top: 30px;">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio</th>
+            <th>SubTotal</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $queryDetalle = "SELECT
+                    p.nombre,
+                    dv.cantidad,
+                    dv.precio,
+                    dv.subTotal
+                    FROM
+                    ventas AS v
+                    INNER JOIN detalleVentas AS dv ON dv.idVenta = v.id
+                    INNER JOIN productos AS p ON p.id = dv.idProd
+                    WHERE
+                    v.id = '$idVenta'";
+        $resDetalle = mysqli_query($con, $queryDetalle);
+        $total = 0;
+        while ($row = mysqli_fetch_assoc($resDetalle)) {
+            $total = $total + $row['subTotal'];
+        ?>
+            <tr>
+                <td><?php echo $row['nombre'] ?></td>
+                <td><?php echo $row['cantidad'] ?></td>
+                <td><?php echo "$".money_format("%i",$row['precio']); ?></td>
+                <td><?php echo "$".money_format("%i",$row['subTotal']); ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        <tr>
+            <td colspan="3" class="text-right"  style="text-align: right;">Total:</td>
+            <td><?php echo "$".money_format("%i",$total); ?></td>
+        </tr>
+
+    </tbody>
+</table>
+<?php $html= ob_get_clean(); ?>
+<?php
+include_once "dompdf/autoload.inc.php";
+use Dompdf\Dompdf;
+$pdf=new Dompdf();
+$pdf->loadHtml($html);
+$pdf->setPaper("A4","landingscape");
+$pdf->render();
+$pdf->stream();
+?>
