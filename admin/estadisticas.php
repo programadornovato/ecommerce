384e8e2785173e4459ac69581759d8b309919ea8
Programador Novato <info@programadornovato.com>
2020-02-01 00:33:12 -0600

    Ecommerce con PHP🐘 y MySql🐬 [34.-Reporte de ventas📈]
    Video:
    https://youtu.be/DAFRUZvetwM

--- admin/estadisticas.php
+++ admin/estadisticas.php
@@ -1,3 +1,18 @@
+<?php
+include_once "db_ecommerce.php";
+$con = mysqli_connect($host, $user, $pass, $db);
+
+$queryNumVentas="SELECT COUNT(id) AS num from ventas 
+where fecha BETWEEN DATE( DATE_SUB(NOW(),INTERVAL 7 DAY) ) AND NOW(); ";
+$resNumVentas=mysqli_query($con,$queryNumVentas);
+$rowNumVentas=mysqli_fetch_assoc($resNumVentas);
+
+$queryNumClientes="SELECT COUNT(id) AS num from clientes; ";
+$resNumClientes=mysqli_query($con,$queryNumClientes);
+$rowNumClientes=mysqli_fetch_assoc($resNumClientes);
+
+
+?>    
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
@@ -6,12 +21,6 @@
           <div class="row mb-2">
             <div class="col-sm-6">
               <h1 class="m-0 text-dark">Dashboard</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard v1</li>
-              </ol>
             </div><!-- /.col -->
           </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -23,13 +32,12 @@
         <div class="container-fluid">
           <!-- Small boxes (Stat box) -->
           <div class="row">
-            <div class="col-lg-3 col-6">
+            <div class="col-6">
               <!-- small box -->
               <div class="small-box bg-info">
                 <div class="inner">
-                  <h3>150</h3>
-
-                  <p>New Orders</p>
+                  <h3><?php echo $rowNumVentas['num']; ?></h3>
+                  <p>Ventas en los ultimos 7 dias</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-bag"></i>
@@ -38,46 +46,15 @@
               </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-success">
-                <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                  <p>Bounce Rate</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
+            <div class="col-6">
               <!-- small box -->
               <div class="small-box bg-warning">
                 <div class="inner">
-                  <h3>44</h3>
-
-                  <p>User Registrations</p>
+                  <h3><?php echo $rowNumClientes['num'] ?></h3>
+                  <p>Clientes</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-danger">
-                <div class="inner">
-                  <h3>65</h3>
-
-                  <p>Unique Visitors</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-pie-graph"></i>
                 </div>
                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
