f732203f69d77b0710dd83b8f748968da0b44684
Programador Novato <info@programadornovato.com>
2020-02-01 02:39:25 -0600

    Ecommerce con PHP🐘 y MySql🐬 [35.-Reporte de ventas📈 2da parte]
    Video:
    https://youtu.be/lL65O_Qc9xA

--- admin/panel.php
+++ admin/panel.php
@@ -182,7 +182,7 @@ $modulo=$_REQUEST['modulo']??'';
   <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
-    $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button);
   </script>
   <!-- Bootstrap 4 -->
   <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
